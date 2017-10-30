37b4031b10cb04029960e6740135c41cf3ab2436
Denver Jackson <denvergjackson@gmail.com>
2017-10-30 17:05:10 +0700

    Completing the mobile first ordering part and setting up the country region api. Also doing the validations for the billing details and the steps on that page and ensuring they work. Done the desktop part and tablet part too.

--- public/src/js/app/app.js
+++ public/src/js/app/app.js
@@ -45,4 +45,100 @@ $(function(){
 	});
     
     
+    $(document).on('click','.title .info',function(){
+		$(this).parent().find('span').toggle();
+	});
+	
+	$(document).on('click','.view-order #total',function(){
+		$('.order-summary,.view-order #total .close, .view-order #total .value, .view-order #total .title').toggle();
+	});
+	
+	$(document).on('click','#checkout',function(e){
+		e.preventDefault();
+		$('#order-page').hide();
+		$('#checkout-page').show();
+	});
+	
+	$(document).on('click','#go-back',function(e){
+		e.preventDefault();
+		$('#order-page').show();
+		$('#checkout-page').hide();
+	});
+	
+	$(document).on('click','.sectors.valid .title',function(e){
+		$(this).parent().find('.details').toggle();
+	});
+	
+	/******
+	*
+	* Functionality for stepping through the checkout form.
+	*
+	**********/
+	$(document).on('click','.actions .continue',function(e){
+		e.preventDefault();
+		var $stepGroup = $(this).parent().parent().parent();
+		var $nextStepGroup = $('#'+$(this).attr('data-next'));
+		$(this).parent().parent().find("input[type=text]").each(function() {
+			$(this).parent().find('.required').remove();
+			if($(this).hasClass('needed')) {
+	            if(this.value == '') {
+		            $stepGroup.removeClass('valid');
+	               $('<div class="required">This field is required</div>').insertAfter($(this));
+	            } else {
+		            $(this).parent().find('.required').remove();
+	            }
+            }
+        });
+        if($stepGroup.find('.required').length < 1) {
+	        $stepGroup.find('.details').hide();
+	        $stepGroup.addClass('valid');
+	        if($stepGroup.attr('id') == 'billing-details') {
+		        if($('input#delivery-same').iCheck('update')[0].checked) {
+			        $(this).parent().parent().find("input[type=text]").each(function() {
+						$('#delivery-details input').val(this.value);
+			        });
+		        }
+	        }
+	        $nextStepGroup.find('.details').show();
+	        $('html, body').animate({
+		        scrollTop: $nextStepGroup.offset().top
+		    }, 500); 
+        } else {
+	        $('html, body').animate({
+		        scrollTop: $stepGroup.offset().top
+		    }, 500);  
+        }
+	});
+	
+	$("#billing-country, #delivery-country").selectric().on("change",function()
+  	{
+  	
+  		var countryCode = $(this).val();
+  		
+  		// Populate country select box from battuta API
+	    var url = "https://battuta.medunes.net/api/region/"+countryCode+"/all/?key=43d14e0faafa38c93811e42838c0ef57&callback=?";
+  		$.getJSON(url,function(regions)
+  		{
+  			$("#billing-region option,#delivery-country option").remove();
+		    //loop through regions..
+		    $.each(regions,function(key,region)
+		    { 
+		        $("<option></option>")
+		         				.attr("value",region.region)
+		         				.append(region.region)
+		                     	.appendTo($("#billing-region,#delivery-country"));
+		    });
+		   // Refresh Selectric
+		   $('#billing-region,#delivery-country').selectric('refresh');
+	    }); 
+  	});
+	
+	$(window).scroll(function() { 
+        if ($(window).scrollTop() > 51) {
+            $('.order-summary').addClass('hittop');
+        } else {
+            $('.order-summary').removeClass('hittop');
+        }
+    });
+    
 });   
